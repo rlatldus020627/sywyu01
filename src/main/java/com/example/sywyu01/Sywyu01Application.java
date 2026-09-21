d997339446d59df793cbeae6c03c1aaeab4dbe0e
rlatldus020627 <rlatldus020627@gmail.com>
2026-09-21 15:59:22 +0900

    260921_02클래스명 정리

--- src/main/java/com/example/sywyu01/Sywyu01Application.java
+++ src/main/java/com/example/sywyu01/Sywyu01Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MolpagembApplication {
+public class Sywyu01Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MolpagembApplication.class, args);
+		SpringApplication.run(Sywyu01Application.class, args);
 	}
 
 }
